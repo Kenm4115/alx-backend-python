5c45b9a3f1469bea97ada3e4fbce7a077792fa8e
kenm4115 <kenm4115@gmail.com>
2025-07-11 20:38:10 +0300

    Modified 0-log queiriss

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -9,13 +9,15 @@ Complete the code below by writing a decorator log_queries that logs the SQL que
 
 import sqlite3
 import functools
+from datetime import datetime  
 
-# Decorator to log SQL queries
+# Decorator to log SQL queries with timestamp
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get('query') or (args[0] if args else '')
-        print(f"[LOG] Executing SQL query: {query}")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing SQL query: {query}")
         return func(*args, **kwargs)
     return wrapper
 

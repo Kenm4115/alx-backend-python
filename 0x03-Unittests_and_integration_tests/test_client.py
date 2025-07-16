3dc9d296d42c1ed3d342d92ec6b1e459a5872072
kenm4115 <kenm4115@gmail.com>
2025-07-17 02:30:17 +0300

    Modified test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,28 +1,18 @@
 
 """
-Familiarize yourself with the client.GithubOrgClient class.
-
-In a new test_client.py file, declare the TestGithubOrgClient(unittest.TestCase) class and implement the test_org method.
-
-This method should test that GithubOrgClient.org returns the correct value.
 
 Use @patch as a decorator to make sure get_json is called once with the expected argument but make sure it is not executed.
 
 Use @parameterized.expand as a decorator to parametrize the test with a couple of org examples to pass to GithubOrgClient, in this order:
-
 google
 abc
 Of course, no external HTTP calls should be made.
-
-memoize turns methods into properties. Read up on how to mock a property (see resource).
 
 Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url.
 
 Use patch as a context manager to patch GithubOrgClient.org and make it return a known payload.
 
 Test that the result of _public_repos_url is the expected one based on the mocked payload.
-
-Implement TestGithubOrgClient.test_public_repos to unit-test GithubOrgClient.public_repos.
 
 Use @patch as a decorator to mock get_json and make it return a payload of your choice.
 
@@ -36,20 +26,10 @@ Implement TestGithubOrgClient.test_has_license to unit-test GithubOrgClient.has_
 
 Parametrize the test with the following inputs
 
-repo={"license": {"key": "my_license"}}, license_key="my_license"
-repo={"license": {"key": "other_license"}}, license_key="my_license"
-You should also parameterize the expected returned value.
-
-We want to test the GithubOrgClient.public_repos method in an integration test. That means that we will only mock code that sends external requests.
-
-Create the TestIntegrationGithubOrgClient(unittest.TestCase) class and implement the setUpClass and tearDownClass which are part of the unittest.TestCase API.
-
-Use @parameterized_class to decorate the class and parameterize it with fixtures found in fixtures.py. The file contains the following fixtures:
+Use @parameterized_class to decorate the class and parameterize it with fixtures found in fixtures.py.
 
 org_payload, repos_payload, expected_repos, apache2_repos
 The setupClass should mock requests.get to return example payloads found in the fixtures.
-
-Use patch to start a patcher named get_patcher, and use side_effect to make sure the mock of requests.get(url).json() returns the correct fixtures for the various values of url that you anticipate to receive.
 
 Implement the tearDownClass class method to stop the patcher.
 

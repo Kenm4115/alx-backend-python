e29a275f11751c8b6689447f09e1293107fce486
kenm4115 <kenm4115@gmail.com>
2025-07-17 02:06:48 +0300

    Modified test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,49 +1,4 @@
 
-"""
-Familiarize yourself with the utils.access_nested_map function and understand its purpose. Play with it in the Python console to make sure you understand.
-
-In this task you will write the first unit test for utils.access_nested_map.
-
-Create a TestAccessNestedMap class that inherits from unittest.TestCase.
-
-Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to.
-
-Decorate the method with @parameterized.expand to test the function for following inputs:
-
-nested_map={"a": 1}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a", "b")
-For each of these inputs, test with assertEqual that the function returns the expected result.
-
-The body of the test method should not be longer than 2 lines.
-
-Implement TestAccessNestedMap.test_access_nested_map_exception. 
-Use the assertRaises context manager to test that a KeyError is raised for the following inputs (use @parameterized.expand):
-
-nested_map={}, path=("a",)
-nested_map={"a": 1}, path=("a", "b")
-Also make sure that the exception message is as expected.
-
-Familiarize yourself with the utils.get_json function.
-
-Define the TestGetJson(unittest.TestCase) class
-implement the TestGetJson.test_get_json method to test that utils.get_json returns the expected result.
-
-We don’t want to make any actual external HTTP calls. Use unittest.mock.patch to patch requests.get. 
-Make sure it returns a Mock object with a json method that returns test_payload which you parametrize alongside the test_url
-
-Test that the output of get_json is equal to test_payload.
-
-Read about memoization and familiarize yourself with the utils.memoize decorator.
-
-Implement the TestMemoize(unittest.TestCase) class with a test_memoize method.
-
-Inside test_memoize, define following class
-
-Use unittest.mock.patch to mock a_method. 
-Test that when calling a_property twice, the correct result is returned but a_method is only called once using assert_called_once.
-"""
-
 #!/usr/bin/env python3
 
 import unittest
@@ -53,7 +8,7 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit tests for utils.access_nested_map"""
+    #Unit tests for utils.access_nested_map
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -61,7 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns expected result"""
+        #Test access_nested_map returns expected result
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -69,14 +24,14 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Test access_nested_map raises KeyError for bad path"""
+        #Test access_nested_map raises KeyError for bad path
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), repr(path[-1]))
 
 
 class TestGetJson(unittest.TestCase):
-    """Unit tests for utils.get_json"""
+    #Unit tests for utils.get_json
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -84,7 +39,7 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
-        """Test get_json returns expected payload and calls requests.get"""
+        #Test get_json returns expected payload and calls requests.get
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
@@ -94,10 +49,10 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """Unit tests for utils.memoize"""
+    #Unit tests for utils.memoize
 
     def test_memoize(self):
-        """Test that memoize caches the result of a method"""
+        #Test that memoize caches the result of a method
 
         class TestClass:
             def a_method(self):
